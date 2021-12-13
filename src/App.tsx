38f423c12889b705c4f274aff9b1f3625adc3239
Yongho5580 <70843139+Yongho5580@users.noreply.github.com>
2021-12-13 15:24:18 +0900

    todolist init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import {
   RecoilRoot
 } from 'recoil';
-import CharacterCounter from './components/CharacterCounter';
 
 function App() {
   return (
     <RecoilRoot>
-      <CharacterCounter />
     </RecoilRoot>
   );
 }
